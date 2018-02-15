2deba2bbdc987fe77f96588ce6e71750d0707cf8
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-02-15 10:41:10 +0530

    greater.py
    big or small

---
+++ greater.py
@@ -0,0 +1,9 @@
+x=float(input("enter the number:"))
+y=float(input("enter the number:"))
+z=flaot(input("enter the number:"))
+if(x>y):
+print("x is greater")
+elif(y>z):
+print("y is greater")
+else:
+print("z is greater")
