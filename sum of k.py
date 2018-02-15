f0098f42d16a6728052fb00efd631cdd6ba8876b
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-02-15 16:12:24 +0530

    sum of k.py

---
+++ sum of k.py
@@ -0,0 +1,10 @@
+n=int(input("enter the number"))
+k=int(input("enter the number"))
+11=[]
+s=0
+for i in range(0,n):
+  a=input()
+  ll.append(a)
+for i in range(o,k):
+  s=s+int(ll[i])
+print(s)  
