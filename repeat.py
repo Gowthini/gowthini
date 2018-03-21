e2c74366c3f3ebc17f6822f66adf935a88accf21
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-03-21 17:53:02 +0530

    repeat.py

---
+++ repeat.py
@@ -0,0 +1,7 @@
+x1=input("enter the value of x1:")
+y1=input("enter the value of y1:")
+temp=x1
+x1=y1
+y1=temp
+print("the value of x after swapping:{}".format(x1))
+print("the value of y after swapping:{}".format(y1))
