f6d4d63ac891913cb8fab41d2bf8359465f47e1a
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-03-21 21:37:08 +0530

    special.py

---
+++ special.py
@@ -0,0 +1,12 @@
+string=raw-input("enter sstring")
+count1=0
+count2=0
+for i in string:
+    if(i.isdigit()):
+    count1=count1+1
+    count2=count2+1
+print("number of digit is:")
+print(count1)
+print("number of character is:")
+print(count2)
+    
