c24dd905e4fecf882f45b9cee836969f6022640f
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-03-21 23:55:36 +0530

    lcm.py

---
+++ lcm.py
@@ -0,0 +1,14 @@
+def lcm(x,y):
+    if x>y:
+       greater=x
+    else:
+        greater=y
+    while(true):
+    if((greater % x==0) and(greater % y==0))
+    lcm=greater
+    break
+    greater+=1
+    return lcm
+ num1=int(input("enter first number:"))
+ num2=int(input("enter second number:"))
+ print("the lcm of",num1,"and",num2,"is",lcm(num,num2))
