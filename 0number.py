171cb3192937c6959b85a39452ca89ed38f674a1
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-03-21 17:42:35 +0530

    0number.py

---
+++ 0number.py
@@ -0,0 +1,6 @@
+lst1=[]
+num1=int(input("how many numbers:"))
+for i in range(num1):
+    number=int(input("enter the number"))
+    lst.append(number)
+print(maximum element in the list is:",max(lst1),"/n minimum element in the list is:",min(lst1))
