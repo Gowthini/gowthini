c7aa3cfa27bb6e0d1ff63197cf7b847a6775a9b5
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-03-25 20:48:38 +0530

    re.py

---
+++ re.py
@@ -0,0 +1,9 @@
+def printRepeats(arr, size):
+	print("Repeats elements are ", end = '')
+	for i in range (0, size):
+		for j in range (i + 1, size):
+			if arr[i] == arr[j]:
+				print(arr[i], end = ' ')
+arr = [4, 2, 4, 5, 2, 3, 1]
+arr_size = len(arr)
+printRepeats(arr, arr_size)
