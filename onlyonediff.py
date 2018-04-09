95022ef781c2c4be03a28bc858b0a377f07a5b24
Gowthini <36299236+Gowthini@users.noreply.github.com>
2018-04-09 16:03:54 +0530

    onlyonediff.py

---
+++ onlyonediff.py
@@ -0,0 +1,12 @@
+def nearlyEqual(str1, str2):
+  str1 = input("enter the first letter")
+  str2 = input("enter the Second letter")
+  if str1 == str2:
+      print ("yes")
+  else:
+      print ("No")
+  return nearlyEqual(str1,str2)
+  print (nearlyEqual(str1,str2))
+str1 = ()
+str2 = ()
+print, nearlyEqual(str1, str2)
